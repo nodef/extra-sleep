a81b59bf5a0be132f52772b426577a18b637c89f
Subhajit Sahu <wolfram77@gmail.com>
2022-04-09 12:48:47 +0530

    :turtle: w/ sleep(), sleepSync()

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
-function sleep(ms) {
+/**
+ * Sleep for specified time (async).
+ * @param ms time in milliseconds
+ * @returns nothing
+ */
+export function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 
-// https://www.npmjs.com/package/sleep : Erik Dubbelboer
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Atomics/wait
-function sleepSync(ms) {
+/**
+ * Sleep for specified time.
+ * @param ms time in milliseconds
+ */
+export function sleepSync(ms: number): void {
   var buff = new SharedArrayBuffer(4);
   var view = new Int32Array(buff);
   Atomics.wait(view, 0, 0, ms);
+  // References:
+  // - https://www.npmjs.com/package/sleep
+  // - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Atomics/wait
 }
-console.log('Start sleeping.');
-sleepSync(1000);
-console.log('Done sleeping.');
